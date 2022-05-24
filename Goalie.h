e357cca47367adc936b3e6e0c22bdfdc65569754
fercattaneo <92549981+fercattaneo@users.noreply.github.com>
2022-05-24 20:29:13 -0300

    Arquero

---
+++ Goalie.h
@@ -0,0 +1,19 @@
+#ifndef GOALIE_H
+#define GOALIE_H
+#include <iostream>
+#include <string>
+#include <cstring>
+
+#include "Players.h"
+
+using namespace std;
+
+class goalie : public Players 
+{
+public:
+
+private:
+    
+};
+
+#endif // GOALIE_H
